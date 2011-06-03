2f009d29b6f59de69e46dfa6aec04c9bc3b0d8e2
Lisandro <luckyluke1979@yahoo.com.ar>
2011-06-03 22:44:47 +0200

    Modifications made by Maris to showTrial & added todo list

--- showTrial.m
+++ showTrial.m
@@ -146,69 +146,6 @@ for flp =1 : blankInterval
 
 end
 
-
-
-%% SUBJECTIVE RESPONSES
-% Draw frames for both eyes
-Screen('FillRect',  Exp.Cfg.win, Exp.Cfg.Color.inc);
-Screen('DrawTextures', Exp.Cfg.win, Exp.stimuli.frameTex, [], Exp.stimuli.destFrame);
-Screen('FillRect', Exp.Cfg.win, Exp.Cfg.Color.inc, Exp.stimuli.newRect);
-Screen('DrawText',Exp.Cfg.win, 'Yes', Exp.stimuli.xLeft - 70, Exp.stimuli.yLeft, [0 0 255]);
-Screen('DrawText',Exp.Cfg.win, 'No', Exp.stimuli.xLeft + 50, Exp.stimuli.yLeft, [0 0 255]);
-Screen('DrawText',Exp.Cfg.win, 'Yes', Exp.stimuli.xRight -70, Exp.stimuli.yRight, [0 0 255]);
-Screen('DrawText',Exp.Cfg.win, 'No', Exp.stimuli.xRight +50, Exp.stimuli.yRight, [0 0 255]);
- 
-vbl= Screen('Flip', Exp.Cfg.win,  [], Exp.Cfg.AuxBuffers);
-
-RTflag=0; %Flag to collect only the first response for the trial
-%Here begins to count the time for the RTs
-time2 = GetSecs;
-%Take the time for the duration startAdaptation-startTest
-% Exp.Synch.time(tr)= time2-time1;
-while (RTflag==0)
-    waitTime=0; % interval of time to check for a response inside each flip
-    while (waitTime < (Exp.Cfg.MonitorFlipInterval-0.005) && RTflag==0)
-        [keyIsDown, T, keyCode ] = KbCheck; %#ok
-        if  (keyIsDown)
-            T = GetSecs;
-            Exp.responses.ActualResponse = KbName(keyCode);
-            if iscell(Exp.responses.ActualResponse)
-                Exp.responses.ActualResponse= []; % The subject pressed two simulatneos butons
-            end
-            Exp.responses.RT= T - time2;
-            RTflag=1;
-            if isempty(Exp.responses.ActualResponse)
-                RTflag = 0;
-                % locations = 1: up; 2: down; 3: Left; 4: right;
-            elseif strcmpi(Exp.responses.ActualResponse, 'Up')
-                RTflag = 0;
-            elseif strcmpi(Exp.responses.ActualResponse, 'Down')
-                RTflag = 0;
-            elseif strcmpi(Exp.responses.ActualResponse, 'Left')
-                Exp.Trial(tr, 8) = 3;
-            elseif strcmpi(Exp.responses.ActualResponse, 'Right')
-                Exp.Trial(tr, 8) = 4;
-            elseif strcmpi(Exp.responses.ActualResponse, Exp.addParams.exitKey)
-                % just do nothing
-            else
-                display('Error in coding responses!!!!')
-                return
-            end
-
-            break;
-        end
-        %wait 5ms in between each Check to avoid 'overheating'
-        WaitSecs(0.005);
-        leavingTime= GetSecs;
-        waitTime= leavingTime - vbl;
-    end
-end
-
-while keyIsDown
-    keyIsDown = KbCheck;
-    WaitSecs(0.01);
-end
-
 %% OBJECTIVE RESPONSES: POSITION
 % Draw frames for both eyes
 Screen('FillRect',  Exp.Cfg.win, Exp.Cfg.Color.inc);
@@ -275,7 +212,92 @@ else
     return
 end
 
+while keyIsDown
+    keyIsDown = KbCheck;
+    WaitSecs(0.01);
+end
 
+%% SUBJECTIVE RESPONSES
+% Draw frames for both eyes
+Screen('FillRect',  Exp.Cfg.win, Exp.Cfg.Color.inc);
+Screen('DrawTextures', Exp.Cfg.win, Exp.stimuli.frameTex, [], Exp.stimuli.destFrame);
+Screen('FillRect', Exp.Cfg.win, Exp.Cfg.Color.inc, Exp.stimuli.newRect);
+%Draw fixation point
+    FixdotDims = [Exp.stimuli.xLeft-4, Exp.stimuli.xRight-4; Exp.stimuli.yLeft-4, Exp.stimuli.yRight-4; ...
+    Exp.stimuli.xLeft+4, Exp.stimuli.xRight+4; Exp.stimuli.yLeft+4, Exp.stimuli.yRight+4;]; 
+    Screen('FillOval', Exp.Cfg.win, [255 0 0], FixdotDims);
+
+    randNum = round(rand(1));
+if randNum
+    Screen('DrawText',Exp.Cfg.win, 'Yes', Exp.stimuli.xLeft - 70, Exp.stimuli.yLeft, [0 0 255]);
+    Screen('DrawText',Exp.Cfg.win, 'No', Exp.stimuli.xLeft + 50, Exp.stimuli.yLeft, [0 0 255]);
+    Screen('DrawText',Exp.Cfg.win, 'Yes', Exp.stimuli.xRight -70, Exp.stimuli.yRight, [0 0 255]);
+    Screen('DrawText',Exp.Cfg.win, 'No', Exp.stimuli.xRight +50, Exp.stimuli.yRight, [0 0 255]);
+else
+    Screen('DrawText',Exp.Cfg.win, 'No', Exp.stimuli.xLeft - 70, Exp.stimuli.yLeft, [0 0 255]);
+    Screen('DrawText',Exp.Cfg.win, 'Yes', Exp.stimuli.xLeft + 50, Exp.stimuli.yLeft, [0 0 255]);
+    Screen('DrawText',Exp.Cfg.win, 'No', Exp.stimuli.xRight -70, Exp.stimuli.yRight, [0 0 255]);
+    Screen('DrawText',Exp.Cfg.win, 'Yes', Exp.stimuli.xRight +50, Exp.stimuli.yRight, [0 0 255]);
+end
+
+vbl= Screen('Flip', Exp.Cfg.win,  [], Exp.Cfg.AuxBuffers);
+
+RTflag=0; %Flag to collect only the first response for the trial
+%Here begins to count the time for the RTs
+time2 = GetSecs;
+%Take the time for the duration startAdaptation-startTest
+% Exp.Synch.time(tr)= time2-time1;
+while (RTflag==0)
+    waitTime=0; % interval of time to check for a response inside each flip
+    while (waitTime < (Exp.Cfg.MonitorFlipInterval-0.005) && RTflag==0)
+        [keyIsDown, T, keyCode ] = KbCheck; %#ok
+        if  (keyIsDown)
+            T = GetSecs;
+            Exp.responses.ActualResponse = KbName(keyCode);
+            if iscell(Exp.responses.ActualResponse)
+                Exp.responses.ActualResponse= []; % The subject pressed two simulatneos butons
+            end
+            Exp.responses.RT= T - time2;
+            RTflag=1;
+            if isempty(Exp.responses.ActualResponse)
+                RTflag = 0;
+                % locations- 3: Left, means 'yes' ; 4: Right, means 'no';
+            elseif strcmpi(Exp.responses.ActualResponse, 'Up')
+                RTflag = 0;
+            elseif strcmpi(Exp.responses.ActualResponse, 'Down')
+                RTflag = 0;
+            elseif strcmpi(Exp.responses.ActualResponse, 'Left')
+                if randNum
+                Exp.Trial(tr, 8) = 3; % 'left' is 'yes'
+                else
+                Exp.Trial(tr, 8) = 4; % 'left' is 'no'
+                end
+            elseif strcmpi(Exp.responses.ActualResponse, 'Right')
+                if randNum
+                    Exp.Trial(tr, 8) = 4;
+                else
+                    Exp.Trial(tr, 8) = 3;
+                end
+            elseif strcmpi(Exp.responses.ActualResponse, Exp.addParams.exitKey)
+                % just do nothing
+            else
+                display('Error in coding responses!!!!')
+                return
+            end
+
+            break;
+        end
+        %wait 5ms in between each Check to avoid 'overheating'
+        WaitSecs(0.005);
+        leavingTime= GetSecs;
+        waitTime= leavingTime - vbl;
+    end
+end
+
+while keyIsDown
+    keyIsDown = KbCheck;
+    WaitSecs(0.01);
+end
 %% ITI
 randi = randperm(length(Exp.stimuli.ITI));
 for flp =1 : Exp.stimuli.ITI(randi(1))
